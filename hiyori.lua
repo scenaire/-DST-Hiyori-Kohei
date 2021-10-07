93a1039a37fa6d4036c3abe05b5b8b8c45ae2c19
scenaire <tempesta-psyzeoul@hotmail.com>
2021-10-08 00:10:03 +0700

    add hiyori

---
+++ hiyori.lua
@@ -0,0 +1,6 @@
+-- The character select screen lines
+STRINGS.CHARACTER_TITLES.hiyori = "The Sample Character"
+STRINGS.CHARACTER_NAMES.hiyori = "Hiyori"
+STRINGS.CHARACTER_DESCRIPTIONS.hiyori = "*Perk 1\n*Perk 2\n*Perk 3"
+STRINGS.CHARACTER_QUOTES.hiyori = "\"Quote\""
+STRINGS.CHARACTER_SURVIVABILITY.hiyori = "Slim"
