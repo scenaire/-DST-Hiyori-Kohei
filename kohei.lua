e2b69e8441e41f6bdd49bcd4f640454da37f798d
scenaire <tempesta-psyzeoul@hotmail.com>
2021-10-10 23:34:12 +0700

    add kohei propoties

---
+++ kohei.lua
@@ -0,0 +1,6 @@
+-- The character select screen lines
+STRINGS.CHARACTER_TITLES.hiyori = "The Sample Character"
+STRINGS.CHARACTER_NAMES.hiyori = "Kohei"
+STRINGS.CHARACTER_DESCRIPTIONS.hiyori = "*Perk 1\n*Perk 2\n*Perk 3"
+STRINGS.CHARACTER_QUOTES.hiyori = "\"I just want to be with you\""
+STRINGS.CHARACTER_SURVIVABILITY.hiyori = "Slim"
