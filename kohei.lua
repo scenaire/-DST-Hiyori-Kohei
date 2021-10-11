b26feea79b01289403427174e833a233e1423118
scenaire <tempesta-psyzeoul@hotmail.com>
2021-10-11 19:47:06 +0700

    add title kohei

--- kohei.lua
+++ kohei.lua
@@ -1,5 +1,5 @@
 -- The character select screen lines
-STRINGS.CHARACTER_TITLES.hiyori = "The Sample Character"
+STRINGS.CHARACTER_TITLES.hiyori = "Farm boy"
 STRINGS.CHARACTER_NAMES.hiyori = "Kohei"
 STRINGS.CHARACTER_DESCRIPTIONS.hiyori = "*Perk 1\n*Perk 2\n*Perk 3"
 STRINGS.CHARACTER_QUOTES.hiyori = "\"I just want to be with you\""
