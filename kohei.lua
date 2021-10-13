964285596020564e1959b36d3f1656c1787a76cf
scenaire <tempesta-psyzeoul@hotmail.com>
2021-10-13 16:42:17 +0700

    change name to kohei

--- kohei.lua
+++ kohei.lua
@@ -1,6 +1,6 @@
 -- The character select screen lines
-STRINGS.CHARACTER_TITLES.hiyori = "Farm boy"
-STRINGS.CHARACTER_NAMES.hiyori = "Kohei"
-STRINGS.CHARACTER_DESCRIPTIONS.hiyori = "*Perk 1\n*Perk 2\n*Perk 3"
-STRINGS.CHARACTER_QUOTES.hiyori = "\"I just want to be with you\""
-STRINGS.CHARACTER_SURVIVABILITY.hiyori = "Slim"
+STRINGS.CHARACTER_TITLES.kohei = "Farm boy"
+STRINGS.CHARACTER_NAMES.kohei = "Kohei"
+STRINGS.CHARACTER_DESCRIPTIONS.kohei = "*Perk 1\n*Perk 2\n*Perk 3"
+STRINGS.CHARACTER_QUOTES.kohei = "\"I just want to be with you\""
+STRINGS.CHARACTER_SURVIVABILITY.kohei = "Slim"
